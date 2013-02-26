0ca4f400e3ffcfafae0ecd44352bb7e5bfac31ca
Philip De Smedt <philip.desmedt@gmail.com>
2013-02-26 13:55:43 +0100

    Scaffolded Post resource

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blogging::Application.config.secret_token = 'd6e3052733ddaf8ba1211a9b6908d019183cf37d937c3a65ddd8668a90d417405224cad32a62269e463f485debcd0ebc8e50c1cc1e9eadd3523f11fe580ce937'
