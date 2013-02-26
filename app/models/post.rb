811b1246b95c677084f0d76ed2f91a89286e9df9
Philip De Smedt <philip.desmedt@gmail.com>
2013-02-26 15:38:05 +0100

    Added comments to posts using associations.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
 class Post < ActiveRecord::Base
   attr_accessible :content, :name, :title
 
+  has_many :comments
+  
   validates :content, :presence => true, :length => { :minimum => 20 }
   validates :title, :presence => true
   validates :name, :presence => true
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Blogging::Application.routes.draw do
-  resources :posts
+  resources :posts do
+    resources :comments
+  end
 
   root :to => "posts#index"
 
